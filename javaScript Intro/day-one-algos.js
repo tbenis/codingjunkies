ae9077cbdf255ef21ff0fc793cb3cba099e38ee3
Tambe <benis.tambe@capgemini.com>
2018-01-28 21:26:45 -0600

    jquery

---
+++ javaScript Intro/day-one-algos.js
@@ -0,0 +1,79 @@
+//Print 1-255 - Print all the integers from 1 to 255.
+function numInteger(){
+  var sumall = 0;
+  var sum =0;
+  var integer = [];
+    for (var i = 0; i <=255; i++) {
+        sumall = sumall + i;
+        integer.push(i);
+    }
+    console.log(sumall);
+    console.log(integer);
+}
+
+// numInteger()
+
+
+//Odds 1-255 - Print all odd integers from 1 to 255.
+function Oddnum(){
+  var number = [];
+  for (var i = 1; i <=255; i++) {
+    if (i%2 != 0) {
+      number.push(i)
+    }
+    console.log(number);
+}
+}
+// Oddnum()
+
+//B
+function oddNum(){
+    for
+}
+
+//Print Sum 0-255 - Print integers from 0 to 255, and the sum so far.
+function numInteger(){
+  var sumall = 0;
+  var sum =0;
+  var integer = [];
+    for (var i = 0; i <=255; i++) {
+         sumall = sumall + i;
+        integer.push(i);
+    }
+    console.log(sumall);
+    console.log(integer);
+}
+
+// numInteger()
+
+//B
+
+function sum255(){
+    var sum = 0;
+    for( var i= 0; i <=255; i++){
+        console.log(i);
+        console.log(sum +=i);
+    }
+}
+sum255();
+
+
+//Iterate Array - Print all values in a given array.
+
+//Find Max - Print the largest element in a given array.
+
+//Get Average - Analyze an array's values and print the average.
+
+//Array with Odds - Create an array with odd integers from 1-255.
+
+//Square the Values - Given an array, square each value in the array.
+
+//Greater Than Y - Count the number of array values greater than a given value Y.
+
+//Zero Out Negative Numbers - Set negative array values to zero.
+
+//Max, Min, Average - Given an array, print max, min and average values.
+
+//Shift Array Values - Given an array, shift all values forward, dropping the first value and leaving an extra '0' value at the end.
+
+//Number to String - Replace any negative values in an array with 'Dojo'.
